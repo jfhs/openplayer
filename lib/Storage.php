0dcdfdf9a6109c7c648e4f5cee58a11bd74b5dd4
unknown <jfhs@.(none)>
2011-07-28 00:49:21 +0600

    Make size work for windows and if exec is disabled or du doesnt work

--- lib/Storage.php
+++ lib/Storage.php
@@ -14,9 +14,103 @@ class Storage {
 		return self::$instance?self::$instance:self::$instance = new \Lib\Storage;
 	}
 	
-	public function size() { 
-		$result = explode("\t",exec("du -s ".$this->path),2); 
-		return ($result[1]==$this->path ? intval($result[0])*1024 : -1); 
+	/**
+	 * Calculate the size of a directory by iterating its contents
+	 *
+	 * @author      Aidan Lister <aidan@php.net>
+	 * @version     1.2.0
+	 * @link        http://aidanlister.com/2004/04/calculating-a-directories-size-in-php/
+	 * @param       string   $directory    Path to directory
+	 */
+	private function slow_size() {
+		$path = $this->path;
+	    // Init
+	    $size = 0;
+	 
+	    // Trailing slash
+	    if (substr($path, -1, 1) !== DIRECTORY_SEPARATOR) {
+	        $path .= DIRECTORY_SEPARATOR;
+	    }
+	 
+	    // Sanity check
+	    if (is_file($path)) {
+	        return filesize($path);
+	    } elseif (!is_dir($path)) {
+	        return false;
+	    }
+	 
+	    // Iterate queue
+	    $queue = array($path);
+	    for ($i = 0, $j = count($queue); $i < $j; ++$i)
+	    {
+	        // Open directory
+	        $parent = $i;
+	        if (is_dir($queue[$i]) && $dir = @dir($queue[$i])) {
+	            $subdirs = array();
+	            while (false !== ($entry = $dir->read())) {
+	                // Skip pointers
+	                if ($entry == '.' || $entry == '..') {
+	                    continue;
+	                }
+	 
+	                // Get list of directories or filesizes
+	                $path = $queue[$i] . $entry;
+	                if (is_dir($path)) {
+	                    $path .= DIRECTORY_SEPARATOR;
+	                    $subdirs[] = $path;
+	                } elseif (is_file($path)) {
+	                    $size += filesize($path);
+	                }
+	            }
+	 
+	            // Add subdirectories to start of queue
+	            unset($queue[0]);
+	            $queue = array_merge($subdirs, $queue);
+	 
+	            // Recalculate stack size
+	            $i = -1;
+	            $j = count($queue);
+	 
+	            // Clean up
+	            $dir->close();
+	            unset($dir);
+	        }
+	    }
+	 
+	    return $size;
+	}
+	
+	public function size() {
+		if ((strtoupper(substr(PHP_OS, 0, 3)) === 'WIN')) {
+			if (class_exists('COM', false)) {
+				$obj = new COM ( 'scripting.filesystemobject' );
+				if (is_object($obj)) {
+					$ref = $obj->getfolder ( $this->path );
+			
+					$result = $ref->size;
+			
+					$obj = null;
+				} else {
+					$result = $this->slow_size();
+				}
+			} else {
+				$result = $this->slow_size();
+			}
+		} elseif (function_exists('exec')) {
+			$s = exec("du -s ".$this->path, $ret);
+			if ($ret) {
+				$result = $this->slow_size();
+			} else {
+				$result = explode("\t",$s,2);
+				$result = intval($result[0])*1024;
+				if ($result[1] != $this->path) {
+					$result = $this->slow_size();
+				}
+			}
+		} else {
+			$result = $this->slow_size();
+		}
+		return $result; 
 	}
 	
 	public function save($data, $filename) {
